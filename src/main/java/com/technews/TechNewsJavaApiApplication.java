dc39e649c5d55e957997edeb765975316f1e0376
Ben Grinthal <bgrinthal@gmail.com>
2022-08-09 12:49:31 -0400

    added post controller

--- src/main/java/com/technews/TechNewsJavaApiApplication.java
+++ src/main/java/com/technews/TechNewsJavaApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TechNewsJavaApiProjectApplication {
+public class TechNewsJavaApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TechNewsJavaApiProjectApplication.class, args);
+		SpringApplication.run(TechNewsJavaApiApplication.class, args);
 	}
 
 }
